endlessm/endless-key-flatpak

Invert `DISABLE_` environment variables
It is now reasonable to interpret an environment variable to a False
value, so we can remove double negatives.

src/kolibri_app/globals.py
@@ -17,11 +17,11 @@ APP_DEVELOPER_EXTRAS = getenv_as_bool(
 )
 
 APP_AUTOMATIC_LOGIN = getenv_as_bool(
-    config.PROFILE_ENV_PREFIX + "APP_DISABLE_AUTOMATIC_LOGIN"
+    config.PROFILE_ENV_PREFIX + "APP_AUTOMATIC_LOGIN", default=True
 )
 
 APP_AUTOMATIC_PROVISION = getenv_as_bool(
-    config.PROFILE_ENV_PREFIX + "APP_DISABLE_AUTOMATIC_PROVISION"
+    config.PROFILE_ENV_PREFIX + "APP_AUTOMATIC_PROVISION", default=True
 )
 
 XDG_CURRENT_DESKTOP = os.environ.get("XDG_CURRENT_DESKTOP")

learningequality/kolibri-installer-gnome

src/kolibri_app/globals.py
@@ -19,11 +19,11 @@ APP_DEVELOPER_EXTRAS = getenv_as_bool(
 )
 
 APP_AUTOMATIC_LOGIN = getenv_as_bool(
-    config.PROFILE_ENV_PREFIX + "APP_DISABLE_AUTOMATIC_LOGIN"
+    config.PROFILE_ENV_PREFIX + "APP_AUTOMATIC_LOGIN", default=USE_SYSTEM_INSTANCE
 )
 
 APP_AUTOMATIC_PROVISION = getenv_as_bool(
-    config.PROFILE_ENV_PREFIX + "APP_DISABLE_AUTOMATIC_PROVISION"
+    config.PROFILE_ENV_PREFIX + "APP_AUTOMATIC_PROVISION", default=USE_SYSTEM_INSTANCE
 )
 
 XDG_CURRENT_DESKTOP = os.environ.get("XDG_CURRENT_DESKTOP")